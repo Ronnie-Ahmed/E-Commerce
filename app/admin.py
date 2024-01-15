926abc146477239f32a465c6eabb6191215ebe48
Ronnie-Ahmed <rksraisul@gmail.com>
2024-01-15 20:27:32 +0600

    models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Item, ItemImage,Buyer,Order
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(ItemImage)
+admin.site.register(Buyer)
+admin.site.register(Order)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.core.validators import EmailValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Items'
+     
+    
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    image_url = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.item.name
+    
+    class Meta:
+        verbose_name_plural = 'Item Images'
+    
+class Buyer(models.Model):
+    name=models.CharField(max_length=30,null=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE ,null=True)
+    profile_pic=models.ImageField(upload_to='images/')
+    descriptions=models.TextField(max_length=500,blank=True,null=True)
+    dateJoined=models.DateField(blank=True,null=True)
+  
+    
+    def save(self,*args,**kwargs):
+        if not self.dateJoined:
+            self.dateJoined=timezone.now() + timezone.timedelta(hours=6)
+        super().save(*args,**kwargs)
+        
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Buyers'
+        
+        
+class Order(models.Model):
+    buyer=models.ForeignKey(Buyer,on_delete=models.CASCADE)
+    item=models.ForeignKey(Item,on_delete=models.CASCADE)
+    odered_at=models.DateField(blank=True,null=True)
+    delivery_date=models.DateField()
+    
+    def save(self,*args,**kwargs):
+        if not self.odered_at:
+            self.odered_at=timezone.now() + timezone.timedelta(hours=6)
+        super().save(*args,**kwargs)
+        
+    def __str__(self):
+        return f"Item= {self.item.name}  By= {self.buyer}"
+    
+    class Meta:
+        verbose_name_plural='Order'
+    
+    
