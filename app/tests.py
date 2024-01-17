485fe7076b6ca85e0f68b4fb4396b6d1af5c20a9
Ronnie-Ahmed <rksraisul@gmail.com>
2024-01-17 20:10:39 +0600

    tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
+from django.test import TestCase,Client
+from .models import Buyer,Category,Item,Order
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class TestModel(TestCase):
+    def test_1(self):
+        try:
+            # emails=input('Enter your email: ')
+            user=User.objects.create_user(username='ronnie',password='ronnie')
+            buyer=Buyer.objects.create(name='Ronnie',email='rksraisul@gmail.com',user=user)
+            self.assertEqual(str(buyer),'Ronnie')
+            self.assertEqual(buyer.email,"rksraisul@gmail.com")
+            self.assertTrue(isinstance(buyer,User))
+        except Exception as e:
+            self.fail(e)
+
